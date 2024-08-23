40136cc37e08b0691a94f6aef427e46523d6e0d8
Mustafa Ahmed Raza <syed.talha@weuno.co>
2024-08-23 10:37:47 +0500

    home page created

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/home', (req, res) => {
+  res.send('Hi this is home')
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
